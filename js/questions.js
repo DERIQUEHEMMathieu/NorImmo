b0e441d37fde433649deb9c6ded3b301e8e5583e
orochefeuille <rochefeuille.o@gmail.com>
2020-08-21 18:49:30 +0200

    Feature: accordeon divs for questions page

--- js/questions.js
+++ js/questions.js
@@ -1,1 +1,10 @@
-alert("ccou");
+let cardsHeader = document.getElementsByClassName("card-header");
+let arrows = document.querySelectorAll(".card span");
+
+
+for (let i = 0 ; i < cardsHeader.length; i++) {
+    cardsHeader[i].addEventListener('click', () => {
+        cardsHeader[i].nextElementSibling.classList.toggle("d-none");
+        cardsHeader[i].children[1].innerText == "+" ? cardsHeader[i].children[1].innerText = "-" : cardsHeader[i].children[1].innerText = "+";
+    });
+ }
