a0d4980760146412f9dbbb66ae4db5a7fcde39f8
Kevin Laguel <laguel.kev.pro@gmail.com>
2020-08-24 09:39:51 +0200

    Add event with addEventListener on carousel

--- js/main.js
+++ js/main.js
@@ -2,22 +2,38 @@
 let slide = ['img/maison1.jpg', 'img/maison2.jpg', 'img/maison3.jpg', 'img/maison4.jpg'];
 let numero = 0;
 var i= 0;
-let time = 5000;
+let time = 4000;
 
+/**
+ * 
+//  * @param {Number} sens - Valeurs possibles (-1, 1)
+//  */
+// function ChangeSlide(sens) {
+//     numero = numero + sens;
+//     if (numero < 0)
+//         numero = slide.length - 1;
+//     if (numero > slide.length - 1)
+//         numero = 0;
+//     document.getElementById("img").src = slide[numero];
+// }
+document.querySelector("#slider div:first-of-type").addEventListener("click", ()=>{ // on recupere le boutton en js et on lui applique une fonction au click
+    numero--;
+    if(numero < 0){
+        numero = slide.length -1;
+    }
+    document.getElementById("img").src = slide[numero]
+})
 
-function ChangeSlide(sens) {
-    numero = numero + sens;
-    if (numero < 0)
-        numero = slide.length - 1;
-    if (numero > slide.length - 1)
+document.querySelector("#slider div:last-of-type").addEventListener("click", ()=>{// on recupere le boutton en js et on lui applique une fonction au click
+    numero++;
+    if(numero > slide.length -1){
         numero = 0;
-    document.getElementById("img").src = slide[numero];
-}
-
+    }
+    document.getElementById("img").src = slide[numero]
+})
 
 function AutoChange(){
     document.getElementById("img").src  = slide[i]; // Initialise la premiere source de l'image a slide[0]
-
     if (i < slide.length -1){ //Parcourir le tableau 
         i++;
     }
@@ -28,3 +44,6 @@ function AutoChange(){
 }
 
 window.onload = AutoChange; // Lancer la fonction quand la page est chargée 
+
+
+
